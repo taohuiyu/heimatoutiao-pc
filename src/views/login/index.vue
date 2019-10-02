de8fd88b2bce88bd52cb4fb08ebce9d92ac27533
tao <1264994050@qq.com>
2019-10-02 21:30:38 +0800

    黑马头条PC-登录模块-页面布局及样式

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,6 +1,33 @@
 <template>
-  <div>
-      我是登录页面
+  <div class="login">
+      <!-- 使用elementUI组件 el-card -->
+      <el-card class="login-card">
+          <!-- 匿名插槽 -->
+          <div class="title">
+              <img src="../../assets/img/logo_index.png" alt="">
+          </div>
+          <!-- 表单 => el-form 包裹 -->
+          <el-form style="margin-top:20px">
+              <el-form-item>
+                  <!-- 手机号 -->
+                  <el-input ></el-input>
+              </el-form-item>
+              <el-form-item>
+                  <!-- 验证码 -->
+                  <el-input style="width:65%"></el-input>
+                  <!-- 发送验证码 -->
+                  <el-button style="float:right">发送验证码</el-button>
+              </el-form-item>
+              <el-form-item>
+                  <!-- 同意选项 -->
+                  <el-checkbox>我已阅读并同意用户协议和隐私条款</el-checkbox>
+              </el-form-item>
+              <el-form-item>
+                  <!--登录按钮 -->
+                    <el-button type="primary" style="width:100%">登录</el-button>
+              </el-form-item>
+          </el-form>
+      </el-card>
   </div>
 </template>
 
@@ -10,6 +37,23 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang="less" scoped>
+.login{
+    background-image: url("../../assets/img/login_bg.jpg");
+    height: 100vh;
+    background-size: cover;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    .login-card{
+        width: 440px;
+        height: 340px;
+        .title{
+            text-align: center;
+            img{
+                height: 45px;
+            }
+        }
+    }
+}
 </style>
