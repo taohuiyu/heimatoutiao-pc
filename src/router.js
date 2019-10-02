36b2a281afcb263ab4e75a7d80df159e2bd7594a
tao <1264994050@qq.com>
2019-10-02 20:51:11 +0800

    黑马头条PC-初始化项目-页面分析-新建登录和主页

--- src/router.js
+++ src/router.js
@@ -1,13 +1,22 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/home'
+import Home from './views/home/index.vue'
+import Login from './views/login'
 Vue.use(Router)
 export default new Router({
   routes: [
     // 一级路由
     {
       path: '/',
+      redirect: '/home'
+    },
+    {
+      path: '/home',
       component: Home
+    },
+    {
+      path: '/login',
+      component: Login
     }
     // {
     //   path: '/about',
